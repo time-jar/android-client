9a1b6fea5084b4166330e0e620d6d297e180bda2
melissazg <zhangouyu@gmail.com>
2023-12-10 04:04:06 +0100

    login and signup views

---
+++ app/src/main/java/com/timejar/app/screens/LoginScreen.kt
@@ -0,0 +1,247 @@
+package com.timejar.app.screens
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.Column
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.timejar.app.R
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginScreen()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen() {
+    var email by remember { mutableStateOf(TextFieldValue("")) }
+    var password by remember { mutableStateOf(TextFieldValue("")) }
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
+    val checkedState = remember { mutableStateOf(false) }
+
+    val onSignInButtonClicked: (String, String) -> Unit = {email, password ->
+        Log.i("LoginScreen", "Email: $email, Password: $password")
+    }
+    val onForgotPasswordButtonClicked: () -> Unit = {}
+    val onSignUpButtonClicked: () -> Unit = {}
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color(0xFFE5E5E5))
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(horizontal = 24.dp, vertical = 32.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.mipmap.ic_launcher),
+                contentDescription = "Time Jar Logo",
+                modifier = Modifier.padding(vertical = 24.dp)
+                    .size(100.dp)
+            )
+
+            Text (
+                text = stringResource(id = R.string.welcome_back),
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.SansSerif,
+                fontSize = 28.sp,
+                color = Color(0xFF393F45)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = stringResource(id = R.string.sign_in_to_continue),
+                fontWeight = FontWeight.Normal,
+                fontFamily = FontFamily.SansSerif,
+                fontSize = 16.sp,
+                color = Color(0xFF393F45)
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.label_email_address),
+                        color = Color(0xFFABB3BB))
+                },
+                placeholder = { Text(text = stringResource(id = R.string.hint_enter_email)) },
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color(0xFF91B3B4),
+                    unfocusedBorderColor = Color(0xFFABB3BB)
+                ),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Email Icon",
+                        tint = Color(0xFF91B3B4)
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+            val description = if (passwordVisible) "Hide password" else "Show password"
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.label_password),
+                        color = Color(0xFFABB3BB))
+                },                placeholder = { Text(text = stringResource(id = R.string.hint_enter_password)) },
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color(0xFF91B3B4),
+                    unfocusedBorderColor = Color(0xFFABB3BB)
+                ),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Email Icon",
+                        tint = Color(0xFF91B3B4)
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = if (passwordVisible) VisualTransformation.None
+                else PasswordVisualTransformation(),
+            )
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(
+                        checked = checkedState.value,
+                        colors = CheckboxDefaults.colors(
+                            uncheckedColor = Color(0xFF91B3B4),
+                            checkedColor = Color(0xFF91B3B4)
+                        ),
+                        onCheckedChange = { checkedState.value = it })
+                    Text(
+                        text = stringResource(id = R.string.remember_me),
+                        fontSize = 14.sp,
+                        color = Color(0xFFABB3BB))
+                }
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Text(
+                    text = stringResource(id = R.string.forgot_password),
+                    fontSize = 14.sp,
+                    color = Color(0xFF91B3B4),
+                    modifier = Modifier.clickable { onForgotPasswordButtonClicked() }
+                )
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Button(
+                onClick = { onSignInButtonClicked(email.text, password.text) },
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFF91B3B4)),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.enter),
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 20.sp,
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(id = R.string.do_not_have_account),
+                    color = Color(0xFFABB3BB),
+                    fontSize = 12.sp
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = stringResource(id = R.string.sign_up),
+                    color = Color(0xFF91B3B4),
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.clickable { onSignUpButtonClicked() }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun LoginScreenPreview() {
+    LoginScreen()
+}
---
+++ app/src/main/java/com/timejar/app/screens/SignUpScreen.kt
@@ -0,0 +1,247 @@
+package com.timejar.app.screens
+
+import android.util.Log
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.timejar.app.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignUpScreen() {
+    var name by remember { mutableStateOf(TextFieldValue("")) }
+    var email by remember { mutableStateOf(TextFieldValue("")) }
+    var password by remember { mutableStateOf(TextFieldValue("")) }
+    var confirmPassword by remember { mutableStateOf(TextFieldValue("")) }
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
+
+    val onSignUpButtonClicked: (String, String) -> Unit = {email, password ->
+        Log.i("LoginScreen", "Email: $email, Password: $password")
+    }
+
+    val onSignInButtonClicked: () -> Unit = {}
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color(0xFFE5E5E5))
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(horizontal = 24.dp, vertical = 32.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.mipmap.ic_launcher),
+                contentDescription = "Time Jar Logo",
+                modifier = Modifier.padding(vertical = 24.dp)
+                    .size(100.dp)
+            )
+
+            Text (
+                text = stringResource(id = R.string.welcome_back),
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.SansSerif,
+                fontSize = 28.sp,
+                color = Color(0xFF393F45)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = stringResource(id = R.string.create_an_account),
+                fontWeight = FontWeight.Normal,
+                fontFamily = FontFamily.SansSerif,
+                fontSize = 16.sp,
+                color = Color(0xFF393F45)
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            OutlinedTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.name_label),
+                        color = Color(0xFFABB3BB)
+                    )
+                },
+                placeholder = { Text(text = stringResource(id = R.string.hint_enter_name)) },
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color(0xFF91B3B4),
+                    unfocusedBorderColor = Color(0xFFABB3BB)
+                ),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = "Person Icon",
+                        tint = Color(0xFF91B3B4)
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.hint_enter_email),
+                        color = Color(0xFFABB3BB)
+                    )
+                },
+                placeholder = { Text(text = stringResource(id = R.string.hint_enter_email)) },
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color(0xFF91B3B4),
+                    unfocusedBorderColor = Color(0xFFABB3BB)
+                ),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = "Email Icon",
+                        tint = Color(0xFF91B3B4)
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.label_password),
+                        color = Color(0xFFABB3BB)
+                    )
+                },                placeholder = { Text(text = stringResource(id = R.string.hint_enter_password)) },
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color(0xFF91B3B4),
+                    unfocusedBorderColor = Color(0xFFABB3BB)
+                ),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Lock Icon",
+                        tint = Color(0xFF91B3B4)
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = if (passwordVisible) VisualTransformation.None
+                else PasswordVisualTransformation()
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            OutlinedTextField(
+                value = confirmPassword,
+                onValueChange = { confirmPassword = it },
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.confirm_password_label),
+                        color = Color(0xFFABB3BB)
+                    )
+                },
+                placeholder = { Text(text = stringResource(id = R.string.hint_confirm_password)) },
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color(0xFF91B3B4),
+                    unfocusedBorderColor = Color(0xFFABB3BB)
+                ),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Lock Icon",
+                        tint = Color(0xFF91B3B4)
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = if (passwordVisible) VisualTransformation.None
+                else PasswordVisualTransformation(),
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = { onSignUpButtonClicked(email.text, password.text) },
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFF91B3B4)),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+            ) {
+                Text(
+                    text = stringResource(id = R.string.sign_up),
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 20.sp,
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = stringResource(id = R.string.already_have_account),
+                    color = Color(0xFFABB3BB),
+                    fontSize = 12.sp
+                )
+                Spacer(modifier = Modifier.width(5.dp))
+                Text(
+                    text = stringResource(id = R.string.sign_in),
+                    color = Color(0xFF91B3B4),
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.clickable { onSignInButtonClicked() }
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SignUpScreenPreview() {
+    SignUpScreen()
+}
