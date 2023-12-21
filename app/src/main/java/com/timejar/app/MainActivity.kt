50bae1df596d0514c377a385b0eea593b9b6457e
melissazg <zhangouyu@gmail.com>
2023-12-21 06:20:04 +0100

    regenerated secret key

--- app/src/main/java/com/timejar/app/MainActivity.kt
+++ app/src/main/java/com/timejar/app/MainActivity.kt
@@ -4,7 +4,7 @@ import NavGraph
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
