a34c6d28fb1833c2b34eb59f4173403d15e87052
mk2376 <mk2376@student.uni-lj.si>
2024-01-08 22:13:47 +0100

    Fixed detecting third-party launchers into blacklist

--- app/src/main/java/com/timejar/app/sensing/app_activity/BlackListedApps.kt
+++ app/src/main/java/com/timejar/app/sensing/app_activity/BlackListedApps.kt
@@ -12,42 +12,45 @@ fun getBlacklistedApps(context: Context): List<String> {
     val apps = packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
     val blacklistedApps = mutableListOf<String>()
 
+    // Add specific apps manually
     blacklistedApps.add("com.timejar.app")
     blacklistedApps.add("com.google.android.apps.nexuslauncher")
 
+    val launcherApps = getLauncherApps(packageManager)
+
     apps.forEach { app ->
         // System apps
-        if (app.flags and ApplicationInfo.FLAG_SYSTEM != 0) {
-            if ((app.sourceDir.startsWith("/system/app/") || app.sourceDir.startsWith("/system/priv-app/")) &&
-                (app.flags and ApplicationInfo.FLAG_UPDATED_SYSTEM_APP == 0)) {
+        if ((app.flags and ApplicationInfo.FLAG_SYSTEM != 0) &&
+            (app.sourceDir.startsWith("/system/app/") || app.sourceDir.startsWith("/system/priv-app/")) &&
+            (app.flags and ApplicationInfo.FLAG_UPDATED_SYSTEM_APP == 0)) {
 
-                Log.d("system app", app.packageName)
-
-                blacklistedApps.add(app.packageName)
-            }
-        }
-
-        // Check for launcher apps
-        val launcherIntent = packageManager.getLaunchIntentForPackage(app.packageName)
-        if (launcherIntent?.categories?.contains(Intent.CATEGORY_HOME) == true) {
-
-            Log.d("launcher app", app.packageName)
-
+            Log.d("system app", app.packageName)
             blacklistedApps.add(app.packageName)
         }
 
-        // Check for debug apps
+        // Launcher apps
+        if (launcherApps.contains(app.packageName)) {
+            Log.d("launcher app", app.packageName)
+            blacklistedApps.add(app.packageName)
+        }
+
+        // Debug apps
         if (app.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0) {
-
             Log.d("debug app", app.packageName)
-
             blacklistedApps.add(app.packageName)
         }
     }
 
     val blacklistedAppsFiltered = blacklistedApps.distinct() // Remove duplicates
-
     Log.d("getBlacklistedApps", blacklistedAppsFiltered.toString())
 
     return blacklistedAppsFiltered
+}
+
+fun getLauncherApps(packageManager: PackageManager): List<String> {
+    val intent = Intent(Intent.ACTION_MAIN).apply {
+        addCategory(Intent.CATEGORY_HOME)
+    }
+    val resolveInfos = packageManager.queryIntentActivities(intent, 0)
+    return resolveInfos.mapNotNull { it.activityInfo?.packageName }
 }
